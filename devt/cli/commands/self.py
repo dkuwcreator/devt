cc46034f55e4fed7dc760898d14db6d1de5c7626
Derk Kappelle <derk.kappelle@uw-api.com>
2025-02-26 03:03:29 +0100

    small change

--- devt/cli/commands/self.py
+++ devt/cli/commands/self.py
@@ -1,18 +1,19 @@
 import typer
+from devt.config_manager import APP_NAME
 from devt import __version__
 
-self_app = typer.Typer(help="DevT self management commands")
+self_app = typer.Typer(help=f"{APP_NAME} self management commands")
 
 @self_app.command("version")
 def self_version():
     """
     Displays the current version of DevT.
     """
-    typer.echo(f"DevT version: {__version__}")
+    typer.echo(f"{APP_NAME} version: {__version__}")
 
 @self_app.command("upgrade")
 def self_upgrade():
     """
     Checks for updates and installs the latest version of DevT.
     """
-    typer.echo("DevT upgraded successfully.")
+    typer.echo(f"{APP_NAME} upgraded successfully.")
